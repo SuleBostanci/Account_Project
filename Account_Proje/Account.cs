39ebbcdbbe7e77452fd1bf1016834a6988830fd1
Damla Sarıca <sulebostanci1@gmail.com>
2025-05-03 23:14:16 +0300

    FirstCommit

---
+++ Account_Proje/Account.cs
@@ -0,0 +1,64 @@
+﻿namespace Account_Proje;
+public class Account
+{
+    public string Ibannumarası { get; set; }
+    public string KişiAdı { get; set; }
+    public double GelenPara { get; set; }
+    public double GidenPara { get; set; }
+    public double ToplamPara { get; set; }
+
+
+    public void setIbannumarası(string Ibannumaras)
+    {
+        Ibannumarası = Ibannumaras;
+    }
+    public void setKişiadı(string KişiAdı)
+    {
+        KişiAdı = KişiAdı;
+    }
+    public void setGelenPara(string GelenPara)
+    {
+        GelenPara = GelenPara;
+    }
+    public void setGidenPara(double GidenPara)
+    {
+        GidenPara = GidenPara;
+        {
+
+            if (GidenPara >= 1000 && GidenPara < 2000)
+            {
+                GidenPara += 4;
+
+            }
+            else if (GidenPara >= 2000 && GidenPara < 10000)
+            {
+                GidenPara += 8;
+
+            }
+            else if (GidenPara >= 10000 && GidenPara < 20000)
+            {
+                GidenPara += 15;
+
+            }
+            else if ((GidenPara >= 20000))
+            {
+                GidenPara *= 1.08;
+
+            }
+        }
+    }
+    public void setToplamPara(double ToplamPara)
+    {
+        ToplamPara = ToplamPara;
+    }
+
+
+
+
+    public override string ToString()
+    {
+        return $"İban numarası{Ibannumarası},Kişi adı{KişiAdı},Gelen para{GelenPara},Giden para {GidenPara},Toplam para {ToplamPara}";
+    }
+}
+
+
---
+++ Account_Proje/Program.cs
@@ -0,0 +1,34 @@
+﻿//Account adında class oluşturunuz
+//account sınıfının propertyleri 
+//●	Iban numarası (string)
+//●	Kişi adı
+//●	Gelen para
+//●	Giden para
+//●	Toplam para
+
+//eğer giden para değeri 1000 tl üzerinde ise ekstradan 4 tl
+//2000 tl üzerindeyse 8 tl 
+//10000 tl üzerindeyse 15 tl 
+//20000 tl nin üzerinde ise %8 ekstra dan eklensin
+
+//●	Toplam para değeri gelen para - giden para şeklinde hesaplanmalıdır
+//Konu : Encapsulation
+using Account_Proje;
+
+Account a = new Account()
+{
+    Ibannumarası = "262213425",
+    KişiAdı = "Hesap",
+    GelenPara = 13000,
+    GidenPara = 3500,
+    ToplamPara = 9500,
+};
+
+//Console.WriteLine(a);
+//a.setGidenPara(a.GidenPara);
+
+//// ToplamPara'yı hesaplıyoruz
+//a.setToplamPara(a.ToplamPara);
+
+//// Bilgileri ekrana yazdırıyoruz
+Console.WriteLine(a);
